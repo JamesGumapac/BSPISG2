3604e2e830caf1814e854f032cfbc1f82b776636
james <jgumapac@bspny.com>
2022-11-30 00:53:14 +0800

    coding the update in line item if the vendor changes

--- src/FileCabinet/SuiteScripts/ISG/bsp_isg_cs_po_get_item_contract_price.js
+++ src/FileCabinet/SuiteScripts/ISG/bsp_isg_cs_po_get_item_contract_price.js
@@ -35,7 +35,49 @@ define(["N/record", "N/search", "./Lib/bsp_isg_get_item_contract_plan.js"], /**
               fieldId: "custbody_bsp_isg_transmission_acct_num",
               value: accountNumber,
             });
-   
+          console.log(" isTP " + isTP)
+          for(let i = 0; i < currentRecord.getLineCount({sublistId: 'item'}); i++) {
+           currentRecord.selectLine({sublistId: 'item',line: i})
+            
+            const itemId = currentRecord.getCurrentSublistValue({
+              sublistId: "item",
+              fieldId: "item",
+              line: i
+            });
+           const quantity = currentRecord.getCurrentSublistValue({
+             sublistId: "item",
+             fieldId: "quantity",
+             line: i
+           });
+            const cost = util.lookForContractCost(itemId, accountNumber);
+            console.log("Cost: " + cost);
+            if(cost){
+              currentRecord.setCurrentSublistValue({
+                sublistId: "item",
+                fieldId: "rate",
+                value: cost,
+                ignoreFieldChange: true
+              });
+              currentRecord.setCurrentSublistValue({
+                sublistId: "item",
+                fieldId: "amount",
+                value: Number(cost) * Number(quantity),
+                ignoreFieldChange: true
+              });
+              currentRecord.commitLine({sublistId: "item"})
+             // currentRecord.commitLine({sublistId: "item"})
+              // currentRecord.setSublistValue({
+              //   sublistId: "item",
+              //   fieldId: "rate",
+              //   line: i,
+              //   value: cost,
+              // });
+        
+            }
+          
+         
+            console.log('FiledChanged Entity ItemId: ' + itemId);
+          }
         }
       }
 
