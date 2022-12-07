13e16f4610fc5c79d2fe69c6d33e585cee7b7476
james <jgumapac@bspny.com>
2022-12-08 00:25:09 +0800

    added functinality that will source the PO vendor from line item if it does not exist in the IF rec

--- src/FileCabinet/SuiteScripts/ISG/bsp_isg_ue_if_set_vendor_line.js
+++ src/FileCabinet/SuiteScripts/ISG/bsp_isg_ue_if_set_vendor_line.js
@@ -6,6 +6,59 @@ define(["N/record", "N/search"], /**
  * @param{record} record
  * @param{search} search
  */ (record, search) => {
+  const afterSubmit = (context) => {
+    const functionName = "afterSubmit";
+
+    try {
+      const recId = context.newRecord.id;
+      const ifRec = record.load({
+        type: record.Type.ITEM_FULFILLMENT,
+        id: recId,
+      });
+      const soId = ifRec.getValue("createdfrom");
+      const soRec = record.load({
+        type: record.Type.SALES_ORDER,
+        id: soId,
+        isDynamic: true,
+      });
+
+      for (let i = 0; i < ifRec.getLineCount({ sublistId: "item" }); i++) {
+        const orderline = ifRec.getSublistValue({
+          sublistId: "item",
+          fieldId: "orderline",
+          line: i,
+        });
+        const poId = ifRec.getSublistValue({
+          sublistId: "item",
+          fieldId: "createdpo",
+          line: i,
+        });
+        if (poId) {
+          ifRec.setSublistValue({
+            sublistId: "item",
+            fieldId: "custcol_bsp_isg_po_vendor",
+            line: i,
+            value: vendorSearch(poId),
+          });
+        } else {
+          // if no poId is provided in if line item check the SO line
+          const poVendor = checkPOfromSO(soRec, orderline - 1);
+          if (poVendor) {
+            ifRec.setSublistValue({
+              sublistId: "item",
+              fieldId: "custcol_bsp_isg_po_vendor",
+              line: i,
+              value: poVendor,
+            });
+          }
+        }
+      }
+      ifRec.save({ ignoreMandatoryFields: true });
+    } catch (e) {
+      log.error(functionName, e.message);
+    }
+  };
+
   /**
    * Look for the vendor ID
    * @param poId
@@ -30,48 +83,44 @@ define(["N/record", "N/search"], /**
       });
       return true;
     });
-    log.debug("vendor", vendor);
     return vendor;
   }
 
   /**
-   * Defines the function definition that is executed after record is submitted.
-   * @param {Object} scriptContext
-   * @param {Record} scriptContext.newRecord - New record
-   * @param {Record} scriptContext.oldRecord - Old record
-   * @param {string} scriptContext.type - Trigger type; use values from the context.UserEventType enum
-   * @since 2015.2
+   * Check if the corresponding line in the SO is a special order
+   * @param soRec
+   * @param line
+   * @returns {*}
    */
-  const afterSubmit = (context) => {
-    const functionName = "afterSubmit";
+  function checkPOfromSO(soRec, line) {
+    let poVendor;
+    poVendor = soRec.getSublistValue({
+      sublistId: "item",
+      fieldId: "povendor",
+      line: line,
+    });
 
-    try {
-      const recId = context.newRecord.id;
-      const ifRec = record.load({
-        type: record.Type.ITEM_FULFILLMENT,
-        id: recId,
+    if (!poVendor) {
+      //if no po vendor set check if there's a po record set in so line
+      const isSpecialOrder = soRec.getSublistValue({
+        sublistId: "item",
+        fieldId: "isspecialorderline",
+        line: line,
       });
-      for (let i = 0; i < ifRec.getLineCount({ sublistId: "item" }); i++) {
-        const poId = ifRec.getSublistValue({
+      log.debug("isSpecialOrder: ", { line, isSpecialOrder });
+      if (isSpecialOrder === "T") {
+        const createdPO = soRec.getCurrentSublistValue({
           sublistId: "item",
           fieldId: "createdpo",
-          line: i,
         });
-
-        if (poId) {
-          ifRec.setSublistValue({
-            sublistId: "item",
-            fieldId: "custcol_bsp_isg_po_vendor",
-            line: i,
-            value: vendorSearch(poId),
-          });
+        if (createdPO) {
+          poVendor = vendorSearch(createdPO);
         }
       }
-      ifRec.save({ ignoreMandatoryFields: true });
-    } catch (e) {
-      log.error(functionName, e.message);
     }
-  };
 
-  return {  afterSubmit };
+    return poVendor;
+  }
+
+  return { afterSubmit };
 });
