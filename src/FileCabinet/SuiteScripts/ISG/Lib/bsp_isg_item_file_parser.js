7f40f09fb2af7052ab3a29a4c2918c1eec275f14
james <jgumapac@bspny.com>
2023-01-03 22:54:46 +0800

    debug creation of unit of measure

--- src/FileCabinet/SuiteScripts/ISG/Lib/bsp_isg_item_file_parser.js
+++ src/FileCabinet/SuiteScripts/ISG/Lib/bsp_isg_item_file_parser.js
@@ -1,3 +1,4 @@
+8;
 /**
  * @NApiVersion 2.1
  */
@@ -101,37 +102,38 @@ define(["N/file", "N/record", "N/search", "N/runtime"], /**
     }
   }
 
-  /**
-   * get the column provided from the feed
-   * @returns {string[]}
-   */
-  function getColumns() {
-    return [
-      "id",
-      "title",
-      "description",
-      "image_link",
-      "link",
-      "price",
-      "quantity",
-      "product_type",
-      "brand",
-      "google_product_category",
-      "condition",
-      "availability",
-      "mpn",
-      "color",
-      "size",
-      "gtin",
-      "shipping_height",
-      "shipping_length",
-      "shipping_width",
-      "shipping_weight",
-      "product_weight",
-      "shipping",
-      "identifier_exists",
-    ];
-  }
+  //
+  // /**
+  //  * get the column provided from the feed
+  //  * @returns {string[]}
+  //  */
+  // function getColumns() {
+  //   return [
+  //     "id",
+  //     "title",
+  //     "description",
+  //     "image_link",
+  //     "link",
+  //     "price",
+  //     "quantity",
+  //     "product_type",
+  //     "brand",
+  //     "google_product_category",
+  //     "condition",
+  //     "availability",
+  //     "mpn",
+  //     "color",
+  //     "size",
+  //     "gtin",
+  //     "shipping_height",
+  //     "shipping_length",
+  //     "shipping_width",
+  //     "shipping_weight",
+  //     "product_weight",
+  //     "shipping",
+  //     "identifier_exists",
+  //   ];
+  // }
 
   /**
    * check if the multiCurrency feature is enabled
@@ -144,11 +146,284 @@ define(["N/file", "N/record", "N/search", "N/runtime"], /**
   }
 
   /**
+   * Create new vendor
+   * @param vendorName
+   * @returns {number|*}
+   */
+  function createVendor(vendorName) {
+    const vendorRec = record.create({
+      type: record.Type.VENDOR,
+      isDynamic: true,
+    });
+    vendorRec.setValue({
+      fieldId: "companyname",
+      value: vendorName,
+    });
+    return vendorRec.save({
+      ignoreMandatoryFields: true,
+    });
+  }
+
+  /**
+   * Create vendor sublist
+   * @param itemId
+   * @param vendorAssociations
+   * @returns {number|*}
+   */
+  function createVendorSublist(itemId, vendorAssociations) {
+    log.debug("createVendorSublist", vendorAssociations);
+    try {
+      const itemRec = record.load({
+        type: record.Type.INVENTORY_ITEM,
+        id: itemId,
+        isDynamic: true,
+      });
+
+      let vendorId = checkIfVendorExists(vendorAssociations[0].vendor_name);
+      if (vendorId === false) {
+        vendorId = createVendor(vendorAssociations[0].vendor_name);
+      }
+      log.debug("createVendorSublist", vendorId);
+      itemRec.selectNewLine("itemvendor");
+      itemRec.setCurrentSublistValue({
+        sublistId: "itemvendor",
+        fieldId: "vendor",
+        value: vendorId,
+      });
+      itemRec.setCurrentSublistValue({
+        sublistId: "itemvendor",
+        fieldId: "purchaseprice",
+        value: vendorAssociations[0].cost,
+      });
+      itemRec.setCurrentSublistValue({
+        sublistId: "itemvendor",
+        fieldId: "vendorcode",
+        value: vendorAssociations[0].uom,
+      });
+      itemRec.commitLine("itemvendor");
+      return itemRec.save({ ignoreMandatoryFields: true });
+    } catch (e) {
+      log.error("createVendorSublist", e.message);
+    }
+  }
+
+  /**
+   * Check if vendor Exists
+   * @param vendorName
+   * @returns {*|boolean}
+   */
+  function checkIfVendorExists(vendorName) {
+    let vendorId;
+    const vendorSearch = search.create({
+      type: "vendor",
+      filters: [["entityid", "is", vendorName]],
+    });
+    vendorSearch.run().each(function (result) {
+      vendorId = result.id;
+    });
+    return vendorId ? vendorId : false;
+  }
+
+  /**
+   * create initial unit of measurement
+   * @param abbreviation
+   * @returns {number|*}
+   */
+  function createUnitOfMeasure(abbreviation) {
+    try {
+      const rec = record.create({
+        type: "unitstype",
+        isDynamic: true,
+      });
+      rec.selectNewLine("uom");
+      switch (abbreviation) {
+        case "EA":
+          rec.setValue({
+            fieldId: "name",
+            value: "Each"
+          })
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "abbreviation",
+            value: abbreviation,
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "conversionrate",
+            value: 1,
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "pluralabbreviation",
+            value: "EA",
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "pluralname",
+            value: "EACH",
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "baseunit",
+            value: true,
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "unitname",
+            value: "EACH",
+          });
+          break;
+        case "PK":
+          rec.setValue({
+            fieldId: "name",
+            value: "Package"
+          })
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "abbreviation",
+            value: abbreviation,
+          });
+
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "conversionrate",
+            value: 1,
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "pluralabbreviation",
+            value: "PKs",
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "pluralname",
+            value: "Packages",
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "baseunit",
+            value: true,
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "unitname",
+            value: "EACH",
+          });
+          break;
+        case "BX":
+          rec.setValue({
+            fieldId: "name",
+            value: "Box"
+          })
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "abbreviation",
+            value: abbreviation,
+          });
+
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "conversionrate",
+            value: 1,
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "pluralabbreviation",
+            value: "BXs",
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "pluralname",
+            value: "Boxes",
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "baseunit",
+            value: true,
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "unitname",
+            value: "Box",
+          });
+          break;
+        case "KG":
+          rec.setValue({
+            fieldId: "name",
+            value: "Kilogram"
+          })
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "abbreviation",
+            value: abbreviation,
+          });
+
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "conversionrate",
+            value: 1,
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "pluralabbreviation",
+            value: "KGs",
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "pluralname",
+            value: "Kilograms",
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "baseunit",
+            value: true,
+          });
+          rec.setCurrentSublistValue({
+            sublistId: "uom",
+            fieldId: "unitname",
+            value: "Kilogram",
+          });
+          break;
+      }
+      rec.commitLine("uom");
+      return rec.save({ ignoreMandatoryFields: true });
+    } catch (e) {
+      log.error("createUnitOfMeasure", e.message);
+    }
+  }
+
+  /**
+   * Create and return the internal Id of unit of measure in Netsuite
+   * @param abbreviation
+   */
+  function getUnitOfmeasureId(abbreviation) {
+    try {
+      let unitOfMeasureId;
+      const unitstypeSearchObj = search.create({
+        type: "unitstype",
+        filters: [["abbreviation", "is", abbreviation]],
+      });
+      const searchResultCount = unitstypeSearchObj.runPaged().count;
+      if (searchResultCount > 0) {
+        unitstypeSearchObj.run().each(function (result) {
+          unitOfMeasureId = result.id;
+        });
+      } else {
+        unitOfMeasureId = createUnitOfMeasure(abbreviation);
+      }
+      return unitOfMeasureId;
+    } catch (e) {
+      log.error("getUnitOfmeasureId", e.message);
+    }
+  }
+
+  /**
    * Create Item Record
    * @param itemData
    * @returns {number|*}
    */
   function createItem(itemData) {
+    log.debug("itemData", itemData);
     try {
       const lbSettings = getLbSettingsForUploadItem();
       const itemRec = record.create({
@@ -157,10 +432,7 @@ define(["N/file", "N/record", "N/search", "N/runtime"], /**
       });
 
       if (itemData.price) {
-        const basedPrice = itemData.price.substring(
-          0,
-          itemData.price.lastIndexOf(" ")
-        );
+        const basedPrice = itemData.price.toFixed(2);
         const isMultiCurrencyEnabled = checkIfMultiCurrencyEnabled();
         let priceSublistId;
         if (isMultiCurrencyEnabled === true) {
@@ -182,11 +454,8 @@ define(["N/file", "N/record", "N/search", "N/runtime"], /**
           sublistId: priceSublistId,
         });
       }
-      if (itemData.product_weight) {
-        const weight = itemData.product_weight.substring(
-          0,
-          itemData.product_weight.lastIndexOf(" ")
-        );
+      if (itemData.shipping_weight) {
+        const weight = itemData.shipping_weight.toFixed(2);
         itemRec.setValue({
           fieldId: "weight",
           value: +weight,
@@ -227,15 +496,21 @@ define(["N/file", "N/record", "N/search", "N/runtime"], /**
           value: itemData.title,
         });
 
-      itemData.brand &&
+      itemData.uom &&
+        itemRec.setValue({
+          fieldId: "unitstype",
+          value: getUnitOfmeasureId(itemData.uom),
+        });
+
+      itemData.manufacturer_name &&
         itemRec.setValue({
           fieldId: "manufacturer",
-          value: itemData.brand,
+          value: itemData.manufacturer_name,
         });
 
       itemRec.setValue({
         fieldId: "itemid",
-        value: itemData.id,
+        value: itemData.sku,
       });
 
       itemRec.setValue({
@@ -243,7 +518,12 @@ define(["N/file", "N/record", "N/search", "N/runtime"], /**
         value: false,
       });
 
-      return itemRec.save({ ignoreMandatoryFields: true });
+      let itemId = itemRec.save({ ignoreMandatoryFields: true });
+      if (itemData.vendor_associations.length > 0) {
+        if (itemData.uom === itemData.vendor_associations[0].uom) {
+          createVendorSublist(itemId, itemData.vendor_associations);
+        }
+      }
     } catch (e) {
       log.error(
         "functionName: createItem",
@@ -255,7 +535,10 @@ define(["N/file", "N/record", "N/search", "N/runtime"], /**
   return {
     getItemObj: getItemObj,
     createItem: createItem,
+    createVendor:createVendor,
+    checkIfVendorExists: checkIfVendorExists,
     getLbSettingsForUploadItem: getLbSettingsForUploadItem,
     ifItemExists: ifItemExists,
+    createVendorSublist: createVendorSublist
   };
 });
--- src/FileCabinet/SuiteScripts/ISG/bsp_isg_mr_create_item.js
+++ src/FileCabinet/SuiteScripts/ISG/bsp_isg_mr_create_item.js
@@ -3,64 +3,136 @@
  * @NScriptType MapReduceScript
  */
 define([
-	"N/file",
-	"N/runtime",
-	"N/https",
-	"./Lib/bsp_isg_item_file_parser.js",
+  "N/file",
+  "N/runtime",
+  "N/https",
+  "./Lib/bsp_isg_item_file_parser.js",
 ], (file, runtime, https, BSPItemParser) => {
-	const getInputData = (inputContext) => {
-		const functionName = "getInputData";
-		log.audit(
-			functionName,
-			"************* GET INPUT DATA STARTED *************"
-		);
-		try {
-			const lbIntegrationSettings = BSPItemParser.getLbSettingsForUploadItem();
-			const lbResponse = https.get({
-				url: lbIntegrationSettings.url,
-			});
-			let itemInfoContent = lbResponse.body;
-			let itemList = itemInfoContent
-				.split("\n")
-				.map((line) => line.split("\t"));
-			return itemList.slice(1); //remove the column part of the arrays
-		} catch (e) {
-			log.error(functionName, e.message);
-		}
-	};
-	
-	const reduce = (reduceContext) => {
-		const functionName = "reduce";
-		try {
-			const itemValue = reduceContext.values;
-			const itemObj = BSPItemParser.getItemObj(itemValue);
-			log.debug("Reduce: itemObj", itemObj);
-			if (BSPItemParser.ifItemExists(itemObj.id)) {
-				log.debug("ITEM ALREADY EXISTS", itemObj.id);
-			} else {
-				const newItemId = BSPItemParser.createItem(itemObj);
-				if (newItemId) {
-					log.debug("ITEM CREATED SUCCESSFULLY", newItemId);
-				}
-			}
-		} catch (e) {
-			log.error(functionName, e.message);
-		}
-	};
-	
-	const summarize = (summaryContext) => {
-		const functionName = "summarize";
-		try {
-			log.audit(functionName, {
-				UsageConsumed: summaryContext.usage,
-				NumberOfQueues: summaryContext.concurrency,
-				NumberOfYields: summaryContext.yields,
-			});
-			log.audit(functionName, "************ EXECUTION COMPLETED ************");
-		} catch (e) {
-			log.error(functionName, e.message);
-		}
-	};
-	
-	return {getInputData, reduce, summarize};
+  const getInputData = (inputContext) => {
+    const functionName = "getInputData";
+    log.audit(
+      functionName,
+      "************* GET INPUT DATA STARTED *************"
+    );
+    try {
+      const sampleFeed = [
+        {
+          sku: "AAG013200",
+          title:
+            "Lined Notes Pages for Planners/Organizers, 6.75 x 3.75, White Sheets, Undated",
+          price: 4.67,
+          list_price: 7.19,
+          description:
+            "Lined notes pages are loose-leaf for easy insert and removal. High-quality paper features superior ink bleed resistance. Undated format to use anytime for list and notes of all kinds. Punched for fit with any matching size ring planner or organizer. 30 sheets per pack. Dated/Undated: Undated; Academic Year: No; Julian Dates: No; Size (Bound-Side First): 6.75 x 3.75.",
+          keywords:
+            "AT-A-GLANCE® AAG013200, AAG-013200, 089138023792, Lined Notes Pages for Planners/Organizers, 6.75 x 3.75, White Sheets, Undated, Loose Leaf Refills,  Lined Notes,  Personal Organizer,   Calendar,  Agendas,  Annuals,  Appointment Tracking,  Dates,  Dating,  Organizers,  Pages,  Time-Management  ",
+          gtin: "089138023792",
+          uom: "EA",
+          mpn: "013200",
+          unspsc: "44112005",
+          manufacturer_id: "e084730d-cbca-4d31-ae95-14f19294bee1",
+          manufacturer_name: "At-A-Glance",
+          shipping_weight: 0.114,
+          shipping_height: 0.25,
+          shipping_length: 6.813,
+          shipping_width: 3.75,
+          default_vendor_id: "B8802687-B2F2-41bb-B221-9433F85B271E",
+          default_vendor_name: "Essendant",
+          vendor_associations: [],
+        },
+        {
+          sku: "AAG031054005",
+          title:
+            "Black Leather Planner/Organizer Starter Set, 8.5 x 5.5, Black Cover, 12-Month (Jan to Dec): Undated",
+          price: 92.68,
+          list_price: 142.59,
+          description:
+            "Black genuine leather binder has professional look and zipper closure. Planner is undated for flexible planning with a full year of tabbed monthly spreads. Comes with 60 two-page daily planning spreads with hourly appointments 8:00 a.m. to 7:00 p.m. Comes with 60 one-page daily planning pages with half-hourly appointments 7:00 a.m. to 6:30 p.m. Comes with eight weekly planning spreads with hourly appointments 7:00 a.m. to 6:00 p.m. Monday through Friday. Extra features include interior storage pockets, address and phone sections, 24 notetaking pages, a note pad and a pen loop. Binder has 7 rings and holds loose-leaf pages. Dated/Undated: Undated; Calendar Format: Daily; Weekly; Monthly; Page Format: One Day per Page; One Day per Two-Page Spread; One Week per Two-Page Spread; One Month per Two-Page Spread; Academic Year: No.",
+          keywords:
+            "AT-A-GLANCE® AAG031054005, AAG-031054005, 038576373259, Black Leather Planner/Organizer Starter Set, 8.5 x 5.5, Black Cover, 12-Month (Jan to Dec): Undated, Agendas,  Annuals,  Appointment Tracking,  Dates,  Dating,  Organizers,  Pages,  Time-Management  ",
+          gtin: "038576373259",
+          uom: "EA",
+          mpn: "031054005",
+          unspsc: "44112005",
+          manufacturer_id: "e084730d-cbca-4d31-ae95-14f19294bee1",
+          manufacturer_name: "At-A-Glance",
+          shipping_weight: 1.75,
+          shipping_height: 1.688,
+          shipping_length: 10.375,
+          shipping_width: 8.125,
+          default_vendor_id: "B8802687-B2F2-41bb-B221-9433F85B271E",
+          default_vendor_name: "Essendant",
+          vendor_associations: [
+            {
+              vendor_sku: "AAG031054005",
+              vendor_id: "0F9A51F8-5292-4858-8B5B-74250A4552F0",
+              vendor_name: "Test3",
+              uom: "EA",
+              cost: 97.12,
+              min_qty: 1,
+            },
+          ],
+        },
+      ];
+      // const lbIntegrationSettings = BSPItemParser.getLbSettingsForUploadItem();
+      // const lbResponse = https.get({
+      // 	url: lbIntegrationSettings.url,
+      // });
+      // let itemInfoContent = lbResponse.body;
+      // let itemList = itemInfoContent
+      // 	.split("\n")
+      // 	.map((line) => line.split("\t"));
+
+      //	return itemList.slice(1); //remove the column part of the arrays
+      return sampleFeed;
+    } catch (e) {
+      log.error(functionName, e.message);
+    }
+  };
+
+  const reduce = (reduceContext) => {
+    const functionName = "reduce";
+    try {
+      const itemObj = JSON.parse(reduceContext.values);
+      //const itemObj = BSPItemParser.getItemObj(itemValue);
+      log.debug("Reduce: itemObj", itemObj);
+      const itemId = BSPItemParser.ifItemExists(itemObj.sku);
+      if (itemId) {
+        if (itemObj.vendor_associations.length > 0) {
+          log.debug("ITEM ALREADY EXISTS", itemObj.sku);
+          let itemUom = itemObj.uom;
+          let vendorUom = itemObj.vendor_associations[0].uom;
+          if (itemUom === vendorUom) {
+            BSPItemParser.createVendorSublist(
+              itemId,
+              itemObj.vendor_associations
+            );
+          } // update the item vendor sublist
+        }
+      } else {
+        const newItemId = BSPItemParser.createItem(itemObj);
+        if (newItemId) {
+          log.debug("ITEM CREATED SUCCESSFULLY", newItemId);
+        }
+      }
+    } catch (e) {
+      log.error(functionName, e.message);
+    }
+  };
+
+  const summarize = (summaryContext) => {
+    const functionName = "summarize";
+    try {
+      log.audit(functionName, {
+        UsageConsumed: summaryContext.usage,
+        NumberOfQueues: summaryContext.concurrency,
+        NumberOfYields: summaryContext.yields,
+      });
+      log.audit(functionName, "************ EXECUTION COMPLETED ************");
+    } catch (e) {
+      log.error(functionName, e.message);
+    }
+  };
+
+  return { getInputData, reduce, summarize };
 });
