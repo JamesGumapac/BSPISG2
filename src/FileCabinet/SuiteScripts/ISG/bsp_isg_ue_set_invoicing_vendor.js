3dc367ee652599fbc96a0dabdcd8764f40d13229
rodrigoc93 <65789979+rodrigoc93@users.noreply.github.com>
2023-01-12 17:13:19 -0300

    Set Invoicing Vendor

---
+++ src/FileCabinet/SuiteScripts/ISG/bsp_isg_ue_set_invoicing_vendor.js
@@ -0,0 +1,34 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ */
+define(["N/search", "N/record", "N/runtime"],
+
+  (search, record, runtime) => {
+
+  const afterSubmit = (context) => {
+      let newRecord = context.newRecord;
+    try { 
+      vendorbillRec = record.load({
+        type: 'vendorbill',
+        id: newRecord.id,
+    });
+      let vendorId = vendorbillRec.getValue({fieldId: 'entity'});
+      let invoicingVendor =  search.lookupFields({
+          type: 'vendor',
+          id: vendorId,
+          columns: ['custentity_bsp_invoicingvendor']
+         });
+      log.debug('invoicingVendor.custentity_bsp_invoicingvendor[0].value',invoicingVendor.custentity_bsp_invoicingvendor[0].value);
+     if(invoicingVendor != '')
+     {
+      vendorbillRec.setValue({fieldId:'entity', value: invoicingVendor.custentity_bsp_invoicingvendor[0].value})
+     }
+      vendorbillRec.save();
+    } catch (e) {
+      log.error(e.message);
+    }
+  };
+
+  return { afterSubmit };
+});
