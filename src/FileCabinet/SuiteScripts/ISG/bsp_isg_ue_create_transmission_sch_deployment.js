ebc66c11a227316bcb20dffaa10ec80942bd1012
Manuel <mcasaretto@bspny.com>
2023-01-21 16:55:06 -0300

    Updated logic to source client scripts from Suitelets and User Events

--- src/FileCabinet/SuiteScripts/ISG/bsp_isg_ue_create_transmission_sch_deployment.js
+++ src/FileCabinet/SuiteScripts/ISG/bsp_isg_ue_create_transmission_sch_deployment.js
@@ -14,17 +14,22 @@ define(['N/record', 'N/runtime', 'N/search'],
             let functionName = "beforeLoad";
             try{
                 let type = scriptContext.type;
+                let transmitionSchedulerRec = scriptContext.newRecord;
                 log.debug(functionName, JSON.stringify(type));
                 if (type == scriptContext.UserEventType.CREATE || type == scriptContext.UserEventType.COPY) {
-                    log.debug(functionName, "Button for Going to Deployment will not be displayed");
-                }else{
-                    let transmitionSchedulerRec = scriptContext.newRecord;
+                    log.debug(functionName, "Schedule btn not displayed");
+                }else{   
                     let scriptDeploymentID = transmitionSchedulerRec.getValue('custrecord_bsp_transm_schd_deployment_id');
                     if(!scriptDeploymentID){
                         let scriptID = transmitionSchedulerRec.getValue('custrecord_bsp_script_id');
-                        let objScript = runtime.getCurrentScript();
                         let objForm = scriptContext.form;
-                        objForm.clientScriptFileId = objScript.getParameter('custscript_bsp_redirect_button_cs');
+
+                        let clientScriptFileId = search.create({
+                            type: "file",
+                            filters: [["name", "is", "bsp_isg_cs_redirect_to_deployment_btn.js"]],
+                        }).run().getRange({ start: 0, end: 1 })[0].id;
+
+                        objForm.clientScriptFileId = clientScriptFileId;
         
                         objForm.addButton({
                             id : 'custpage_goto_deployment',
@@ -37,8 +42,6 @@ define(['N/record', 'N/runtime', 'N/search'],
                 log.error(functionName, JSON.stringify(error));
             }
         }   
-
-
         return {beforeLoad}
 
     });
